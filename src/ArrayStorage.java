c787c953e6c74ca9a55bfa88c8829a16b7a5fa37
Kirill <kirill.olen@yandex.ru>
2020-05-18 12:16:57 +0300

    Урок 1. Реализация методов для резюмэ.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,53 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, 0, size(), null);
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size()] = resume;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume res = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString() == uuid) {
+                res = storage[i];
+                break;
+            }
+        }
+        return res;
     }
 
     void delete(String uuid) {
+        int num = 0;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString() == uuid) {
+                num = i;
+                break;
+            }
+        }
+        for (int i = num; i < size(); i++) {
+            storage[i] = storage[i + 1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                size++;
+            } else {
+                break;
+            }
+        }
+        return size;
     }
 }
