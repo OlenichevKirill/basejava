fd15a77d61c57dc0a45c2bd3a9dfe1c61782e402
Kirill <kirill.olen@yandex.ru>
2020-07-12 13:47:29 +0300

    Ln 10. xml

--- test/ru/basejava/webapp/storage/XmlPathStorageTest.java
+++ test/ru/basejava/webapp/storage/XmlPathStorageTest.java
@@ -2,8 +2,8 @@ package ru.basejava.webapp.storage;
 
 import ru.basejava.webapp.storage.SerializationStrategy.XmlStreamSerializer;
 
-public class XmlPathStorageTest extends AbstractStorageTest{
+public class XmlPathStorageTest extends AbstractStorageTest {
     public XmlPathStorageTest() {
-        super( new PathStorage(STORAGE_DIR.getAbsolutePath(), new XmlStreamSerializer()));
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new XmlStreamSerializer()));
     }
 }
