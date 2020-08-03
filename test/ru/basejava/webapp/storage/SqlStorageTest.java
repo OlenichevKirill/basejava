3e27f52c858d5d46db4424211dce77345c112ade
Kirill <kirill.olen@yandex.ru>
2020-08-03 16:07:13 +0300

    Lesson13 HW_create SqlStorage and Test

--- test/ru/basejava/webapp/storage/SqlStorageTest.java
+++ test/ru/basejava/webapp/storage/SqlStorageTest.java
@@ -2,7 +2,7 @@ package ru.basejava.webapp.storage;
 
 import ru.basejava.webapp.Config;
 
-public class SqlStorageTest extends AbstractStorageTest{
+public class SqlStorageTest extends AbstractStorageTest {
 
     public SqlStorageTest() {
         super(Config.get().getSqlStorage());
