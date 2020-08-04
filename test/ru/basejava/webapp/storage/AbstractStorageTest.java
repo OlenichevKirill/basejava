894c6c41b739018f1b4d37d267211b5521a09d26
Kirill <kirill.olen@yandex.ru>
2020-08-04 17:31:56 +0300

    Lesson13 HW_refactoring test_class

--- test/ru/basejava/webapp/storage/AbstractStorageTest.java
+++ test/ru/basejava/webapp/storage/AbstractStorageTest.java
@@ -10,21 +10,21 @@ import ru.basejava.webapp.exception.NotExistStorageException;
 import ru.basejava.webapp.model.Resume;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public abstract class AbstractStorageTest {
     protected static final File STORAGE_DIR = Config.get().getStorageDir();
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
 
     protected Storage storage;
 
-    private static final String UUID_1 = "uuid1                               ";
+    private static final String UUID_1 = UUID.randomUUID().toString(); //"uuid1";
     private Resume resume1 = ResumeTestData.createResume(UUID_1, "Tom");
-    private static final String UUID_2 = "uuid2                               ";
+    private static final String UUID_2 = UUID.randomUUID().toString(); //"uuid2";
     private Resume resume2 = ResumeTestData.createResume(UUID_2, "Bill");
-    private static final String UUID_3 = "uuid3                               ";
+    private static final String UUID_3 = UUID.randomUUID().toString(); //"uuid3";
     private Resume resume3 = ResumeTestData.createResume(UUID_3, "Tom");
-    private static final String UUID_4 = "uuid4                               ";
+    private static final String UUID_4 = UUID.randomUUID().toString(); //"uuid4";
     private Resume resume4 = ResumeTestData.createResume(UUID_4, "Kirill");
 
 
@@ -55,6 +55,7 @@ public abstract class AbstractStorageTest {
     public void getAllSorted() {
         List<Resume> actualResumes = storage.getAllSorted();
         List<Resume> expectedResumes = Arrays.asList(resume2, resume1, resume3);
+        expectedResumes.sort(RESUME_COMPARATOR);
         Assert.assertEquals(expectedResumes, actualResumes);
     }
 
--- test/ru/basejava/webapp/storage/AllStorageTest.java
+++ test/ru/basejava/webapp/storage/AllStorageTest.java
@@ -14,6 +14,7 @@ import org.junit.runners.Suite;
         ObjectStreamPathStorageTest.class,
         XmlPathStorageTest.class,
         JsonPathStorageTest.class,
+        DataPathStorageTest.class,
         SqlStorageTest.class})
 public class AllStorageTest {
 }
