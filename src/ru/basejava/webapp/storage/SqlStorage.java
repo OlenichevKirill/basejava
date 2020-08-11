774072101ba642c9538facf6d21b6b469f12568e
Kirill <kirill.olen@yandex.ru>
2020-08-11 11:44:49 +0300

    Lesson14 HW_refactoring refactoring getAllSorted

--- src/ru/basejava/webapp/storage/SqlStorage.java
+++ src/ru/basejava/webapp/storage/SqlStorage.java
@@ -111,14 +111,14 @@ public class SqlStorage implements Storage {
                     ResultSet rs = ps.executeQuery();
                     while (rs.next()) {
                         String uuid = rs.getString("uuid");
-                        resumeMap.computeIfAbsent(uuid, k -> {
+                        Resume resume = resumeMap.computeIfAbsent(uuid, k -> {
                             try {
                                 return new Resume(uuid, rs.getString("full_name"));
                             } catch (SQLException e) {
                                 throw new StorageException(e);
                             }
                         });
-                        addContact(resumeMap.get(uuid), rs);
+                        addContact(resume, rs);
                     }
                     return new ArrayList<>(resumeMap.values());
                 });
